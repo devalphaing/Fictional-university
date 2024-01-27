bcf32455561f5d44901a5c4b68fcf2e8fd3ff6e8
Devang Sharma <devangsharma7861@gmail.com>
2024-01-27 15:05:02 +0530

    dynamic Menu added

--- functions.php
+++ functions.php
@@ -1,7 +1,8 @@
 <?php
 
-function university_files() {
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'),'1.0', true );
+function university_files()
+{
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('university_main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -10,7 +11,11 @@ function university_files() {
 
 add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features() {
+function university_features()
+{
+    register_nav_menu('headerMenuLocation', 'Header Menu Location');
+    register_nav_menu('footerMenuLocationOne', 'Footer Menu Location One');
+    register_nav_menu('footerMenuLocationTwo', 'Footer Menu Location Two');
     add_theme_support('title-tag');
 }
 
