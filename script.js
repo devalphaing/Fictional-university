ae83c5044dd5ecdeefa6c51a1ced9fe638871a80
Devang Sharma <devangsharma7861@gmail.com>
2024-02-14 15:38:11 +0530

    api added

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
+
+  document.body.insertAdjacentHTML('beforeend', `
+    <div class="search-overlay">
+    <div class="search-overlay__top">
+      <div class="container">
+        <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+        <input type="text" class="search-term" placeholder="What are you looking for?" id="search-item">
+        <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+      </div>
+    </div>
+  
+    <div class="container">
+      <div id="search-overlay__results"></div>
+    </div>
+  </div>
+`)
+
   let isOverlayOpen = false;
   let isSpinnerRunning = false;
   let inputValue = "";
@@ -25,6 +42,9 @@ document.addEventListener("DOMContentLoaded", function () {
       searchOverlay.classList.add("search-overlay--active");
       document.body.classList.add("body-no-scroll");
       isOverlayOpen = true;
+      setTimeout(() => {
+        inputRef.focus();
+      }, 301)
     });
   });
 
@@ -33,6 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Remove the search-overlay--active class to close the overlay
     searchOverlay.classList.remove("search-overlay--active");
     document.body.classList.remove("body-no-scroll");
+    inputRef.value = '';
     isOverlayOpen = false;
   });
 
@@ -40,12 +61,16 @@ document.addEventListener("DOMContentLoaded", function () {
     if (event.keyCode === 83 && !isOverlayOpen && !isInputFocused()) {
       searchOverlay.classList.add("search-overlay--active");
       document.body.classList.add("body-no-scroll");
+      setTimeout(() => {
+        inputRef.focus();
+      }, 301)
       isOverlayOpen = true;
     }
     // Check if the Esc key is pressed
     if (event.keyCode === 27 && isOverlayOpen) {
       searchOverlay.classList.remove("search-overlay--active");
       document.body.classList.remove("body-no-scroll");
+      inputRef.value = '';
       isOverlayOpen = false;
     }
   });
@@ -65,10 +90,45 @@ document.addEventListener("DOMContentLoaded", function () {
         resultsDiv.innerHTML = '<div class="spinner-loader"></div>';
         isSpinnerRunning = true;
       }
-      timeTyper = setTimeout(() => {
-        resultsDiv.innerHTML = "<div>Results will come here...</div>";
+      timeTyper = setTimeout(async () => {
+        try {
+
+          //delete below code later on
+          const promises = [
+            axios.get(`${universityData.root_url}/wp-json/wp/v2/posts?search=${inputValue}`),
+            axios.get(`${universityData.root_url}/wp-json/wp/v2/pages?search=${inputValue}`)
+          ];
+      
+          // Execute all promises concurrently
+          const [resDataPosts, resDataPages] = await Promise.all(promises);
+
+          const resData = resDataPosts.data.concat(resDataPages.data);
+          console.log(resData, ' data ----');
+
+          let html = `
+            <h2 class="search-overlay__section-title">Search Results:</h2>
+          `;
+
+          html += resData.length ? `<ul class="link-list min-list">` : '<p>No Information Found!</p>';
+
+          resData.forEach((data) => {
+            html += `
+              <li>
+                <a href="${data?.link}">
+                  ${data?.title?.rendered}
+                </a>
+                ${data?.type === 'post' ? 'by ' + data?.authorName : ''}
+              </li>
+            `;
+          });
+          html += resData.length ? `</ul>`: '';
+          resultsDiv.innerHTML = html;
+        } catch (err) {
+          console.error(err);
+          resultsDiv.innerHTML = '<h2 class="search-overlay__section-title">Error</h2>';
+        }
         isSpinnerRunning = false;
-      }, 2000);
+      }, 750);
     }
   });
 
